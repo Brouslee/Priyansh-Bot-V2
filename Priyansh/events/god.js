d67ffe06b672f507bc8c2c4561a3bef6c1e60b88
Brouslee <148523328+Brouslee@users.noreply.github.com>
2025-08-01 11:21:57 +0200

    Update god.js

--- Priyansh/events/god.js
+++ Priyansh/events/god.js
@@ -1,50 +1,53 @@
 module.exports.config = {
 	name: "god",
-	eventType: ["log:unsubscribe","log:subscribe","log:thread-name"],
+	eventType: ["log:unsubscribe", "log:subscribe", "log:thread-name"],
 	version: "1.0.0",
 	credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩",
-	description: "Record bot activity notifications!",
-    envConfig: {
-        enable: true
-    }
+	description: "تسجيل إشعارات أنشطة البوت!",
+	envConfig: {
+		enable: true
+	}
 };
 
 module.exports.run = async function({ api, event, Threads }) {
-    const logger = require("../../utils/log");
-    if (!global.configModule[this.config.name].enable) return;
-    var formReport =  "=== Bot Notification ===" +
-                        "\n\n» Thread mang ID: " + event.threadID +
-                        "\n» Action: {task}" +
-                        "\n» Action created by userID: " + event.author +
-                        "\n» " + Date.now() +" «",
-        task = "";
-    switch (event.logMessageType) {
-        case "log:thread-name": {
-            const oldName = (await Threads.getData(event.threadID)).name || "Name does not exist",
-                    newName = event.logMessageData.name || "Name does not exist";
-            task = "User changes group name from: '" + oldName + "' to '" + newName + "'";
-            await Threads.setData(event.threadID, {name: newName});
-            break;
-        }
-        case "log:subscribe": {
-            if (event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) task = "The user added the bot to a new group!";
-            break;
-        }
-        case "log:unsubscribe": {
-            if (event.logMessageData.leftParticipantFbId== api.getCurrentUserID()) task = "The user kicked the bot out of the group!"
-            break;
-        }
-        default: 
-            break;
-    }
+	const logger = require("../../utils/log");
+	if (!global.configModule[this.config.name].enable) return;
 
-    if (task.length == 0) return;
+	let formReport =  "=== إشعار بوت ===" +
+					  "\n\n» معرف المجموعة: " + event.threadID +
+					  "\n» الحدث: {task}" +
+					  "\n» تم بواسطة معرف المستخدم: " + event.author +
+					  "\n» التوقيت: " + Date.now() + " «",
+		task = "";
 
-    formReport = formReport
-    .replace(/\{task}/g, task);
-  var god = "100037743553265";
+	switch (event.logMessageType) {
+		case "log:thread-name": {
+			const oldName = (await Threads.getData(event.threadID)).name || "لا يوجد اسم سابق",
+				  newName = event.logMessageData.name || "لا يوجد اسم جديد";
+			task = "قام المستخدم بتغيير اسم المجموعة من: '" + oldName + "' إلى '" + newName + "'";
+			await Threads.setData(event.threadID, { name: newName });
+			break;
+		}
+		case "log:subscribe": {
+			if (event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID()))
+				task = "قام المستخدم بإضافة البوت إلى مجموعة جديدة!";
+			break;
+		}
+		case "log:unsubscribe": {
+			if (event.logMessageData.leftParticipantFbId == api.getCurrentUserID())
+				task = "قام المستخدم بطرد البوت من المجموعة!";
+			break;
+		}
+		default:
+			break;
+	}
 
-    return api.sendMessage(formReport, god, (error, info) => {
-        if (error) return logger(formReport, "[ Logging Event ]");
-    });
+	if (task.length == 0) return;
+
+	formReport = formReport.replace(/\{task}/g, task);
+	const god = "100037743553265";
+
+	return api.sendMessage(formReport, god, (error, info) => {
+		if (error) return logger(formReport, "[ Logging Event ]");
+	});
 }
