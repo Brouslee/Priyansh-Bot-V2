9ab4c011e402fd889e69e049a45b77f275d3595d
Brouslee <148523328+Brouslee@users.noreply.github.com>
2025-08-01 11:31:05 +0200

    Update autosent.js

--- Priyansh/events/autosent.js
+++ Priyansh/events/autosent.js
@@ -3,25 +3,30 @@ module.exports.config = {
  eventType: [],
  version: "0.0.1",
  credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩",
- description: "Listen events"
+ description: "استماع للأحداث"
 };
 
 module.exports.run = async({ event, api, Threads, Users }) => {
-const moment = require("moment-timezone");
-time = moment.tz('Asia/Kolkata').format('HH:mm:ss');
-var cantsend = [];
+    const moment = require("moment-timezone");
+    time = moment.tz('Asia/Kolkata').format('HH:mm:ss');
+    var cantsend = [];
     var allThread = global.data.allThreadID || [];
+
     if (time == "16:52:00") {
-    for (const idThread of allThread) {
-        if (isNaN(parseInt(idThread)) || idThread == event.threadID) ""
-        else {
-            api.sendMessage("test" + args.join(" ") , idThread, (error, info) => {
-                if (error) cantsend.push(idThread);
-            });
+        for (const idThread of allThread) {
+            if (isNaN(parseInt(idThread)) || idThread == event.threadID) ""
+            else {
+                api.sendMessage("رسالة اختبارية" + args.join(" ") , idThread, (error, info) => {
+                    if (error) cantsend.push(idThread);
+                });
+            }
         }
-      }
-    for (var id of global.config.ADMINBOT) {
-          api.sendMessage(`Error when automatically sending messages to threads:\n${cantsend.join("\n")}`,id);
+
+        for (var id of global.config.ADMINBOT) {
+            api.sendMessage(
+                `حدث خطأ أثناء إرسال الرسائل تلقائيًا للمجموعات التالية:\n${cantsend.join("\n")}`,
+                id
+            );
+        }
     }
-  }
-                                                                                          }
+}
