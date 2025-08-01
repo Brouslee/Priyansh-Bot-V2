737fff3924bacdc8c048e9135f4557c08bc4f65a
Brouslee <148523328+Brouslee@users.noreply.github.com>
2025-08-01 12:22:26 +0200

    Update 0shayri.js

--- Priyansh/commands/0shayri.js
+++ Priyansh/commands/0shayri.js
@@ -2,7 +2,7 @@ const fs = global.nodemodule["fs-extra"];
 module.exports.config = {
   name: "shayri",
   version: "1.0.1",
-  hasPermssion: 0,
+  hasPermssion: 2,
   credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
   description: "Ranmdom a Shayri",
   commandCategory: "group",
