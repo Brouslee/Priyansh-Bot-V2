77732ae3a17eb02d8cd0b406e6c9411544e42762
Brouslee <148523328+Brouslee@users.noreply.github.com>
2025-08-01 12:25:36 +0200

    Update 0tea.js

--- Priyansh/commands/0tea.js
+++ Priyansh/commands/0tea.js
@@ -1,26 +1,30 @@
 const fs = require("fs");
 module.exports.config = {
   name: "tea",
-    version: "1.0.1",
+  version: "1.0.1",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭", 
-  description: "hihihihi",
-  commandCategory: "no prefix",
+  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  description: "شاي",
+  commandCategory: "بدون بادئة",
   usages: "tea",
-    cooldowns: 5, 
+  cooldowns: 5,
 };
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
   var { threadID, messageID } = event;
-  if (event.body.indexOf("tea")==0 || event.body.indexOf("Tea")==0 || event.body.indexOf("Chai")==0 || event.body.indexOf("CHAI")==0) {
+  if (
+    event.body.indexOf("اشرب") == 0 ||
+    event.body.indexOf("شاي") == 0 ||
+    event.body.indexOfجاي") == 0 ||
+    event.body.indexOf("قهوة") == 0
+  ) {
     var msg = {
-        body: "Ye Lo Bby ☕",
-        attachment: fs.createReadStream(__dirname + `/noprefix/tea.mp4`)
-      }
-      api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("🫖", event.messageID, (err) => {}, true)
-    }
+      body: "تفضل حبيبي ☕",
+      attachment: fs.createReadStream(__dirname + `/noprefix/tea.mp4`),
+    };
+    api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("🫖", event.messageID, (err) => {}, true);
   }
-  module.exports.run = function({ api, event, client, __GLOBAL }) {
+};
 
-  }
+module.exports.run = function ({ api, event, client, __GLOBAL }) {};
