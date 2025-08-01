2ed0e481d9b1f3b307e88877b4fd10de013769e3
Brouslee <148523328+Brouslee@users.noreply.github.com>
2025-08-01 12:34:13 +0200

    Update Offbot.js

--- Priyansh/commands/Offbot.js
+++ Priyansh/commands/Offbot.js
@@ -3,12 +3,22 @@ module.exports.config = {
 	version: "1.0.0",
 	hasPermssion: 2,
 	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
-	description: "turn the bot off",
-	commandCategory: "system",
+	description: "إيقاف تشغيل البوت",
+	commandCategory: "النظام",
 	cooldowns: 0
-        };
-module.exports.run = ({event, api}) =>{
-    const permission = ["100037743553265", "100037743553265"];
-  	if (!permission.includes(event.senderID)) return api.sendMessage("[ ERR ] You don't have permission to use this command, This Command Only For Priyansh", event.threadID, event.messageID);
-  api.sendMessage(`[ OK ] ${global.config.BOTNAME} Bot are now turned off.`,event.threadID, () =>process.exit(0))
-}
+};
+
+module.exports.run = ({ event, api }) => {
+	const صلاحيات = ["100037743553265", "100037743553265"];
+	if (!صلاحيات.includes(event.senderID))
+		return api.sendMessage(
+			"[ خطأ ] ليس لديك صلاحية لاستخدام هذا الأمر، هذا الأمر فقط لـ Priyansh",
+			event.threadID,
+			event.messageID
+		);
+	api.sendMessage(
+		`[ تم ] تم إيقاف تشغيل بوت ${global.config.BOTNAME} الآن.`,
+		event.threadID,
+		() => process.exit(0)
+	);
+};
